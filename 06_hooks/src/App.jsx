6f3f1765a490015bc74daea70a5a871f3a1b7c49
ahmi5567 <ah55677435@gmail.com>
2026-03-10 15:03:42 +0500

    refactor App component to utilize useCallback and useRef, and clean up unused code

--- 06_hooks/src/App.jsx
+++ 06_hooks/src/App.jsx
@@ -1,52 +1,89 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  // const [data, setData] = useState(null);
 
   // useEffect(() => {
   //   console.log("use effect");
   // }, [count]);
-  async function getWeatherData() {
-    try {
-      setLoading(true);
-      const res = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts/1"
-      );
-      setData(res.data);
-      console.log(res);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  }
-  useEffect(() => {
-    getWeatherData();
+  // async function getWeatherData() {
+  //   try {
+  //     setLoading(true);
+  //     const res = await axios.get(
+  //       "https://jsonplaceholder.typicode.com/posts/1"
+  //     );
+  //     setData(res.data);
+  //     console.log(res);
+  //   } catch (error) {
+  //     console.log(error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }
+  // useEffect(() => {
+  //   getWeatherData();
+  // }, []);
+
+  // use memo hook
+  // const doubleCount = useMemo(() => {
+  //   console.log("double increment");
+  //   return count * 2;
+  // }, [count]);
+  // console.log(count);
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const inputRef = useRef(null);
+  const divRef = useRef(null);
+
+  const increment = useCallback(() => {
+    // console.log(count);
+    setCount((prev) => prev + 1);
   }, []);
 
-  const increment = () => {
-    console.log(count);
-    setCount((prev) => prev - 1);
-    setCount((prev) => prev - 1);
-    console.log(count);
+  const handleFocus = () => {
+    console.log("by ref");
+    inputRef.current.focus();
   };
-  // console.log(count);
+  const scrollToDiv = () => {
+    divRef.current.scrollIntoView();
+  };
 
+  // const doubleCount = count * 2;
+  // const doubleCount = useMemo(() => {
+  //   console.log("double increment");
+  //   return count * 2;
+  // }, [count]);
+  // console.log(count);
+  // console.log(doubleCount);
   return (
     <>
-      <h1>Today we will discuss about the hooks</h1>
+      <div style={{ scrollBehavior: "smooth" }}>
+        <h1>Today we will discuss about the hooks</h1>
+        <button onClick={scrollToDiv}>Scroll</button>
+        <div
+          style={{
+            height: "1000px",
+            backgroundColor: "red",
+            borderColor: "red",
+          }}
+        >
+          ds
+        </div>
 
-      <p>{count}</p>
-      <button onClick={increment}>Increment</button>
-      <br />
+        <div ref={divRef}>Target</div>
+      </div>
+      {/* <p>{count}</p> */}
+      {/* <p>Count useMemo: {doubleCount}</p> */}
+      {/* <p>{text}</p> */}
+      {/* <input type="text" ref={inputRef} placeholder="write anything" /> */}
+      {/* <button onClick={handleFocus}>Focus</button> */}
       {/* <button onClick={getWeatherData}>
         {loading ? "Getting data" : "Get Data"}
       </button> */}
-      {data ? (
+      {/* {data ? (
         <div>
           <h2>Todo Data</h2>
           <p>Title: {data.title}</p>
@@ -54,7 +91,7 @@ function App() {
         </div>
       ) : (
         <p>Loading data</p>
-      )}
+      )} */}
     </>
   );
 }
